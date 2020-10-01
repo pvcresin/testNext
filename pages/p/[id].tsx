5211d3137b329fd32b09095793364525284053d3
pvcresin <pvcresin0730@gmail.com>
2020-10-01 23:33:38 +0900

    Update render method

--- pages/p/[id].tsx
+++ pages/p/[id].tsx
@@ -4,19 +4,19 @@ import fetch from 'isomorphic-unfetch'
 
 const Post: NextPage<{
   show: {
-    name: string
-    summary: string
-    image: { medium: string }
+    name?: string
+    summary?: string
+    image?: { medium?: string }
   }
 }> = ({ show }) => (
   <>
-    <h1>{show.name}</h1>
-    <p>{show.summary}</p>
-    <img src={show.image.medium} />
+    <h1>{show?.name}</h1>
+    <p>{show?.summary}</p>
+    <img src={show?.image?.medium} />
   </>
 )
 
-Post.getInitialProps = async function(context) {
+Post.getInitialProps = async function (context) {
   const { id } = context.query
   const res = await fetch(`https://api.tvmaze.com/shows/${id}`)
   const show = await res.json()
